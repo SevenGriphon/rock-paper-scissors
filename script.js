70987dda77e28bab1b2299e3bc4eb9aca0b81e5a
SevenGriphon <152780844+SeveneGriphon@users.noreply.github.com>
2025-10-02 14:53:59 +0200

    Add computer choice

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hellooooooooo!")
+function getComputerChoice() {
+    let numberChoice = Math.ceil(Math.random()*3);
+    let choice;
+    switch (numberChoice) {
+        case (1):
+            choice = "rock";
+            break;
+        case (2):
+            choice = "paper";
+            break;
+        case (3):
+            choice = "scissors";
+            break;
+    }
+    return choice;
+}
+
+console.log(getComputerChoice())
