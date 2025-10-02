2aaed5b5b99d20967560b9437dec6a5d4fa2078d
SevenGriphon <152780844+SeveneGriphon@users.noreply.github.com>
2025-10-02 15:09:16 +0200

    Add basic round logic

--- script.js
+++ script.js
@@ -16,12 +16,27 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
-    let choice = prompt("Enter your choice (rock/paper/scissors)")
-    return choice.toLowerCase()
+    let choice = prompt("Enter your choice (rock/paper/scissors)");
+    return choice.toLowerCase();
+}
+
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice){ // Tie
+        alert("Tie!");
+    } else if (
+        playerChoice == "rock" && computerChoice == "paper" ||
+        playerChoice == "paper" && computerChoice == "scissors" ||
+        playerChoice == "scissors" && computerChoice == "rock"
+    ) { // Computer Wins
+        alert("You Lost!");
+    } else {
+        alert("You Won!");
+    }
 }
 
 let playerScore = 0;
 let computerScore = 0;
 
-console.log(getComputerChoice())
-console.log(getPlayerChoice())
+let computerChoice = getComputerChoice();
+let playerChoice = getPlayerChoice();
+playRound(playerChoice, computerChoice)
