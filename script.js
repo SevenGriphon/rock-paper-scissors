c1efc015e6340a130c755329c2d906d8f7b3a6c4
SevenGriphon <152780844+SeveneGriphon@users.noreply.github.com>
2025-10-05 19:13:28 +0200

    Remake system to work with UI
    Instead of running the game 5 times the game starts when player choses
    his item. Moved information about round results and score from allerts
    to UI.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const playerScoreDiv = document.querySelector(".player-score")
+const computerScoreDiv = document.querySelector(".computer-score")
+const roundResultDiv = document.querySelector(".round-result")
+
+let playerScore = 0
+let computerScore = 0
+
 function getComputerChoice() {
     let numberChoice = Math.ceil(Math.random()*3);
     let choice;
@@ -15,35 +22,51 @@ function getComputerChoice() {
     return choice;
 }
 
-function getPlayerChoice(e) {
-    let btn = e.target
-    let choice = btn.id
-    return choice
-}
-
 function capitalizeFirstLetter(string) {
     let firstLetter = string[0].toUpperCase();
     string = firstLetter + string.slice(1);
     return string
 }
 
-function playRound(playerChoice, computerChoice) {
-    if (playerChoice === computerChoice){ // Tie
-        alert("Tie!");
+function getRoundResult(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice){
+        return "tie"
     } else if (
         computerChoice == "rock" && playerChoice == "paper" ||
         computerChoice == "paper" && playerChoice == "scissors" ||
         computerChoice == "scissors" && playerChoice == "rock"
-    ) { // Player Wins
-        alert(`You Won! ${capitalizeFirstLetter(playerChoice)} beats ${capitalizeFirstLetter(computerChoice)}`);
-        playerScore++;
-    } else { // Computer Wins
-        alert(`You Lost! ${capitalizeFirstLetter(computerChoice)} beats ${capitalizeFirstLetter(playerChoice)}`);
-        computerScore++;
+    ) return "win"
+    else return "lose"
+}
+
+function playRound(e) {
+    let btn = e.target
+    let playerChoice = btn.id
+    let computerChoice = getComputerChoice()
+    let result = getRoundResult(playerChoice, computerChoice)
+
+    let fancyPlayerChoice = capitalizeFirstLetter(playerChoice)
+    let fancyComputerChoice = capitalizeFirstLetter(computerChoice)
+    switch (result) {
+        case ("win"):
+            roundResultDiv.textContent = 
+            `You Won! ${fancyPlayerChoice} beats ${fancyComputerChoice}`
+            playerScore++
+            playerScoreDiv.textContent = `Player: ${playerScore}`
+            break
+        case ("lose"):
+            roundResultDiv.textContent = 
+                `You Lost! ${fancyComputerChoice} beats ${fancyPlayerChoice}`
+            computerScore++
+            computerScoreDiv.textContent = `Computer: ${computerScore}`
+            break
+        default:
+            roundResultDiv.textContent = 
+                `Tie! Both chose ${fancyPlayerChoice}`
     }
 }
 
 const choices = document.querySelectorAll(".choice-container button")
 choices.forEach((btn) => {
-    btn.addEventListener("click", getPlayerChoice)
+    btn.addEventListener("click", playRound)
 })
